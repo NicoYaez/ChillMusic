c9424ff26afd417a11a8d24f1b8811cddeb9630b
Nicolás Yáñez <nicoyaez171@gmail.com>
2024-05-08 18:42:36 -0400

    Remove old version

--- events/ready.js
+++
@@ -1,6 +0,0 @@
-const { ActivityType } = require('discord.js')
-
-module.exports = async (client) => {
-    console.log(`Registrado en el cliente ${client.user.username}\n-> Listo en ${client.guilds.cache.size} Servidores para un total de ${client.users.cache.size} usuarios`);
-    client.user.setActivity('/play', { type: ActivityType.Listening });
-};
